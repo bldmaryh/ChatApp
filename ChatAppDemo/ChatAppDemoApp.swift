940fa2844844e15bb44de536241f462f1e698cce
Merih Hatay <merihhatay@gmail.com>
2023-12-28 00:13:34 +0300

    first commit

---
+++ ChatAppDemo/ChatAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppDemoApp.swift
+//  ChatAppDemo
+//
+//  Created by merih on 27.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
